9931ce96d5a2aedb18ea83574a27741056139c0d
USAMAASGHAR <you@example.com>
2018-11-28 22:02:37 +0500

    fianl code

--- 3d.cpp
+++ 3d.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
@@ -24,6 +25,12 @@ double getY()  const
 {
   return y;
 }
+double get_Distance(const two_d &p2)
+{
+  double a;
+   a=pow((pow(x-p2.x,2)+pow(y-p2.y,2)),0.5);
+return a;
+}
 };
 class three_d : public two_d{
 private:
@@ -33,13 +40,27 @@ public:
 
   three_d(double a)          {   z=a;    }
 
+  void setZ(double newZ)     {   z=newZ;        }
+
   double getZ()              {   return z;        }
+  double get_Distance(const three_d &p2)
+  {
+    double a;
+     a=pow((pow(x-p2.x,2)+pow(y-p2.y,2))+pow(z-p2.z,2),0.5);
+  return a;
+  }
 };
+
 int main()
 {
+  three_d p1(23),p2(2);
   three_d obj1;
-  cout<<obj1.getX();
-  cout<<obj1.getY();
-  cout<<obj1.getZ();
+  p1.get_Distance(p2);
+  cout<<p1.get_Distance(p2);
+
+//  obj1.setX(34);
+//  cout<<obj1.getX();
+//  cout<<obj1.getY();
+///  cout<<obj1.getZ();
   return 0;
 }
